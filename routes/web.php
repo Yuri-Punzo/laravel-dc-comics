d9f25b633ff555bc1b3525c885ad6a7c835493fc
Yuri-Punzo <yuripunzo@hotmail.it>
2022-12-21 16:58:46 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('admin/comics', [PostController::class, 'index'])->name('comics.index');
