07ea4e842e254b9e7fc10189c8aef33708e71685
Yuri-Punzo <yuripunzo@hotmail.it>
2022-12-21 15:50:59 +0100

    real initial commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,87 @@
+<div id="site_footer_top">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img src="{{Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div class="col">
+                <img src="{{Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt="">
+                <span>DC MERCHANDISE</span>
+            </div>
+            <div class="col">
+                <img src="{{Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt="">
+                <span>SUBSCRIPTION</span>
+            </div>
+            <div class="col">
+                <img src="{{Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="">
+                <span>COMIC SHOP LOCATOR</span>
+            </div>
+            <div class="col">
+                <img id="svg_img" src="{{Vite::asset('resources/img/buy-dc-power-visa.svg')}}" alt="">
+                <span>DC POWER VISA</span>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="site_footer_center">
+    <div class="container">
+        <div class="row">
+            <div class="col-auto my_col">
+                <h2>DC COMICS</h2>
+                <a href="">Characters</a>
+                <a href="">Comics</a>
+                <a href="">Movies</a>
+                <a href="">TV</a>
+                <a href="">Games</a>
+                <a href="">Videos</a>
+                <a href="">News</a>
+                <h2>SHOP</h2>
+                <a href="">Shop DC</a>
+                <a href="">Shop DC Collectibles</a>
+            </div>
+            <div class="col-auto my_col">
+                <h2>DC</h2>
+                <a href="">Term Of Use</a>
+                <a href="">Privacy Policy(New)</a>
+                <a href="">Ad Choices</a>
+                <a href="">Advertising</a>
+                <a href="">Jobs</a>
+                <a href="">Subscriptions</a>
+                <a href="">Talent Workshops</a>
+                <a href="">CPSC Certificates</a>
+                <a href="">Ratings</a>
+                <a href="">Shop Help</a>
+                <a href="">Contact Us</a>
+            </div>
+            <div class="col-auto my_col">
+                <h2>SITES</h2>
+                <a href="">DC</a>
+                <a href="">MAD Magazine</a>
+                <a href="">DC Kids</a>
+                <a href="">DC Universe</a>
+                <a href="">DC Power Visa</a>
+            </div>
+            <div class="col col_dc_bg">
+
+            </div>
+        </div>
+    </div>
+</div>
+<div id="site_footer_bottom">
+    <div class="container">
+        <div class="row">
+            <div class="col my_col">
+                <span class="my_button">SIGN-UP NOW!</span>
+            </div>
+            <div class="col my_col_end">
+                <span class="follow_us">FOLLOW US</span>
+                <img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt="">
+                <img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt="">
+                <img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt="">
+                <img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt="">
+                <img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt="">
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <div id="site_header">
+        <div class="container">
+            <div class="row">
+                <div class="col-auto">
+                    <div class="logo">
+                        <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="">
+                    </div>
+                </div>
+                <div class="col">
+                    <nav class="my_navbar">
+
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="jumbotron">
+
+        </div>
+    </div>
+</header>
